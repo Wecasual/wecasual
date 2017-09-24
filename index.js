3f7c8a8a046c650ca391b19d556c05550e8a2ebd
RobertG-H <ultraconga@hotmail.com>
2017-09-24 17:08:16 -0400

    Added frontend for payment page, and changed some text on index and signup

--- index.js
+++ index.js
@@ -56,13 +56,14 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(function(req, res, next) {
-    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
-        res.redirect('https://' + req.get('Host') + req.url);
-    }
-    else
-        next();
-});
+// HTTPS redirect (Comment if not working locally)
+// app.use(function(req, res, next) {
+//     if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+//         res.redirect('https://' + req.get('Host') + req.url);
+//     }
+//     else
+//         next();
+// });
 
 //Session middleware
 app.use(session({
