4d56e0356e162b5e0d3560ac6b605b739eea2a24
Wecasual <wecasualteam@gmail.com>
2017-09-24 17:07:19 -0400

    Stripe fix for non registered users

--- index.js
+++ index.js
@@ -56,13 +56,13 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(function(req, res, next) {
-    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
-        res.redirect('https://' + req.get('Host') + req.url);
-    }
-    else
-        next();
-});
+// app.use(function(req, res, next) {
+//     if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+//         res.redirect('https://' + req.get('Host') + req.url);
+//     }
+//     else
+//         next();
+// });
 
 //Session middleware
 app.use(session({
