000e22100ae56b3c951db8b56218828886574423
Wecasual <wecasualteam@gmail.com>
2017-09-24 15:34:45 -0400

    Adding https redirect in develop

--- index.js
+++ index.js
@@ -56,6 +56,14 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(function(req, res, next) {
+    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+    }
+    else
+        next();
+});
+
 //Session middleware
 app.use(session({
     secret: 'aqua secret',
