5bb1ca822cd3c1fffc4ce8058001a4010eadf093
Johnnybib <jgrahamhu@hotmail.com>
2017-10-09 12:50:57 -0400

    Player display on admin

--- public/scripts/create-team-form.js
+++ public/scripts/create-team-form.js
@@ -11,11 +11,13 @@ $(document).ready(function(){
         alert(res.error);
       }
       else if(res.success){
+        var i = 1;
         res.data.forEach(function(ele){
           if(ele.team == 'No team'){
-            $('#player-list').append('<tr class="add-row" id="' + ele.id + '"><th scope="row"><img class="rounded-circle" src=' + ele.avatar + '></th><td>' + ele.displayName +
+            $('#player-list').append('<tr class="add-row" id="' + ele.id + '"><td>' + i + '</td><th scope="row"><img class="rounded-circle" src=' + ele.avatar + '></th><td>' + ele.displayName +
             '</td><td>' + ele.skillLevel + '</td><td>' + ele.playerRequests.player1 + ', ' + ele.playerRequests.player2 + ', ' + ele.playerRequests.player3 + ', ' +
             ele.playerRequests.player4 + '</td></tr>');
+            i++;
           }
         });
       }
--- public/scripts/players.js
+++ public/scripts/players.js
@@ -8,9 +8,6 @@ $(document).ready(function(){
       }
       else if(res.success){
         var i = 1;
-        res.data = res.data.sort(function(a, b) {
-          return compareStrings(a.displayName, b.displayName);
-        })
         res.data.forEach(function(ele){
           $('#player-list').append('<tr><td>' + i + '</td><th scope="row"><img class="rounded-circle" src=' + ele.avatar + '></th><td>' + ele.displayName + '</td><td>' + ele.team + '</td> ' + '</tr>');
           i++;
@@ -19,10 +16,3 @@ $(document).ready(function(){
     }
   });
 });
-function compareStrings(a, b) {
-// Assuming you want case-insensitive comparison
-a = a.toLowerCase();
-b = b.toLowerCase();
-
-return (a < b) ? -1 : (a > b) ? 1 : 0;
-}
