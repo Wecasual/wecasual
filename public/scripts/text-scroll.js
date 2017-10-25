8c62c5fd17f5b6df80a46ba84ec028a578540d9e
Johnnybib <jgrahamhu@hotmail.com>
2017-10-25 15:22:31 -0400

    Scrolling words now work

---
+++ public/scripts/text-scroll.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var elements = new Array('#dota-scroll', '#lol-scroll', '#esports-scroll');
+  var i = 0;
+  $(elements[0]).slideToggle();
+  setInterval( function(){
+    console.log(i);
+    $(elements[i]).slideToggle();
+    setTimeout(function(){
+      if(i + 2 > elements.length)
+      {
+          i = -1;
+      }
+      $(elements[i+1]).slideToggle()
+      i++;
+    }, 500) ;
+  }, 2000 );
+});
