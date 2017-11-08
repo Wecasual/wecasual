971fe5852699730b3a191eebf56c3d9bd289e58a
Johnnybib <jgrahamhu@hotmail.com>
2017-11-08 09:18:15 -0500

    Updated error pages to use res.status

--- index.js
+++ index.js
@@ -463,10 +463,10 @@ function renderPost(req, res) {
 //Error handling
 //Handle 404
 app.use(function(req, res) {
-   res.send('404: Page not Found', 404);
+  res.status(404).send('404: Page not Found');
 });
 
 // Handle 500
 app.use(function(error, req, res, next) {
-   res.send('500: Internal Server Error', 500);
+  res.status(500).send('500: Internal Server Error');
 });
