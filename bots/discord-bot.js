8be68b9ec08e1284881ee332cb23d516d2f5b5cd
Johnnybib <jgrahamhu@hotmail.com>
2017-11-08 17:18:38 -0500

    removed semicolon

--- bots/discord-bot.js
+++ bots/discord-bot.js
@@ -31,7 +31,7 @@ function init(bot, admins){
 function postQuickLink(bot, info){
   bot.sendMessage({
     to: '377100730240335872',
-    message: "@everyone Click here to signup for " + info[0] + " on " + info[1] + ": <http://wecasual.gg/schedule/dota/quickLink?id=" + info[2] + ">";
+    message: "@everyone Click here to signup for " + info[0] + " on " + info[1] + ": <http://wecasual.gg/schedule/dota/quickLink?id=" + info[2] + ">"
   }, function(err){
     if(err){
       console.log(err);
