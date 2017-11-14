19ce2be3f62e3e83843185b0a63b5a73796888f7
Johnnybib <jgrahamhu@hotmail.com>
2017-11-13 20:17:13 -0500

    added error pages

--- index.js
+++ index.js
@@ -464,10 +464,14 @@ function renderPost(req, res) {
 //Error handling
 //Handle 404
 app.use(function(req, res) {
-  res.status(404).send('404: Page not Found');
+  res.status(404)
+  res.render('pages/error', {errorMessage: "404: Page not Found"});
+  //.send('404: Page not Found');
 });
 
 // Handle 500
-// app.use(function(error, req, res, next) {
-//   res.status(500).send('500: Internal Server Error');
-// });
+app.use(function(error, req, res, next) {
+  res.status(500)
+  res.render('pages/error', {errorMessage: "500: Internal Server Error"});
+  //.send('500: Internal Server Error');
+});
