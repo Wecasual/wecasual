a8a956ce6e1ba6075700431bfe03709aaf973882
Johnnybib <jgrahamhu@hotmail.com>
2017-12-14 17:22:26 -0500

    Added audience mode, colour hero, and letter draft game rules

--- public/scripts/gamemode-rules.js
+++ public/scripts/gamemode-rules.js
@@ -2,465 +2,617 @@ console.log('ready');
 var heroes = new Array(
   {
     name: 'Abaddon',
-    colour: 'Blue, Purple'
+    icon: '/images/heroes/abaddon.png',
+    colour: 'blue, purple'
   },
   {
     name: 'Alchemist',
-    colour: 'Yellow, Purple'
+    icon: '/images/heroes/alchemist.png',
+    colour: 'yellow, purple'
   },
   {
     name: 'Ancient Apparition',
-    colour: 'Blue, White'
+    icon: '/images/heroes/ancient_apparition.png',
+    colour: 'blue, white'
   },
   {
     name: 'Anti-Mage',
-    colour: 'Purple'
+    icon: '/images/heroes/antimage.png',
+    colour: 'purple'
   },
   {
     name: 'Arc Warden',
-    colour: 'Blue, Purple'
+    icon: '/images/heroes/arc_warden.png',
+    colour: 'blue, purple'
   },
   {
     name: 'Axe',
-    colour: 'Red'
+    icon: '/images/heroes/axe.png',
+    colour: 'red'
   },
   {
     name: 'Bane',
-    colour: 'Purple'
+    icon: '/images/heroes/bane.png',
+    colour: 'purple'
   },
   {
     name: 'Batrider',
-    colour: 'Yellow, Red'
+    icon: '/images/heroes/batrider.png',
+    colour: 'yellow, red'
   },
   {
     name: 'Beastmaster',
-    colour: 'Yellow'
+    icon: '/images/heroes/beastmaster.png',
+    colour: 'yellow'
   },
   {
     name: 'Bloodseeker',
-    colour: 'Red'
+    icon: '/images/heroes/bloodseeker.png',
+    colour: 'red'
   },
   {
     name: 'Bounty Hunter',
-    colour: 'Yellow'
+    icon: '/images/heroes/bounty_hunter.png',
+    colour: 'yellow'
   },
   {
     name: 'Brewmaster',
-    colour: 'Yellow'
+    icon: '/images/heroes/brewmaster.png',
+    colour: 'yellow'
   },
   {
     name: 'Bristleback',
-    colour: 'Yellow'
+    icon: '/images/heroes/bristleback.png',
+    colour: 'yellow'
   },
   {
     name: 'Broodmother',
-    colour: 'Purple'
+    icon: '/images/heroes/broodmother.png',
+    colour: 'purple'
   },
   {
     name: 'Centaur Warrunner',
-    colour: 'Red'
+    icon: '/images/heroes/centaur_warrunner.png',
+    colour: 'red'
   },
   {
     name: 'Chaos Knight',
-    colour: 'Red'
+    icon: '/images/heroes/chaos_knight.png',
+    colour: 'red'
   },
   {
     name: 'Chen',
-    colour: 'White'
+    icon: '/images/heroes/chen.png',
+    colour: 'white'
   },
   {
     name: 'Clinkz',
-    colour: 'Yellow'
+    icon: '/images/heroes/clinkz.png',
+    colour: 'yellow'
   },
   {
     name: 'Clockwerk',
-    colour: 'Red, White'
+    icon: '/images/heroes/clockwerk.png',
+    colour: 'red, white'
   },
   {
     name: 'Crystal Maiden',
-    colour: 'Blue, White'
+    icon: '/images/heroes/crystal_maiden.png',
+    colour: 'blue, white'
   },
   {
     name: 'Dark Seer',
-    colour: 'Purple'
+    icon: '/images/heroes/dark_seer.png',
+    colour: 'purple'
   },
   {
     name: 'Dark Willow',
-    colour: 'Purple'
+    icon: '/images/heroes/dark_willow.png',
+    colour: 'purple'
   },
   {
     name: 'Dazzle',
-    colour: 'Purple'
+    icon: '/images/heroes/dazzle.png',
+    colour: 'purple'
   },
   {
     name: 'Death Prophet',
-    colour: 'Green, Purple'
+    icon: '/images/heroes/death_prophet.png',
+    colour: 'green, purple'
   },
   {
     name: 'Disruptor',
-    colour: 'Blue, Red'
+    icon: '/images/heroes/disruptor.png',
+    colour: 'blue, red'
   },
   {
     name: 'Doom',
-    colour: 'Red'
+    icon: '/images/heroes/doom.png',
+    colour: 'red'
   },
   {
     name: 'Dragon Knight',
-    colour: 'Red'
+    icon: '/images/heroes/dragon_knight.png',
+    colour: 'red'
   },
   {
     name: 'Drow Ranger',
-    colour: 'Blue'
+    icon: '/images/heroes/drow_ranger.png',
+    colour: 'blue'
   },
   {
     name: 'Earth Spirit',
-    colour: 'Green'
+    icon: '/images/heroes/earth_spirit.png',
+    colour: 'green'
   },
   {
     name: 'Earthshaker',
-    colour: 'Yellow'
+    icon: '/images/heroes/earthshaker.png',
+    colour: 'yellow'
   },
   {
     name: 'Elder Titan',
-    colour: 'Green, Yellow'
+    icon: '/images/heroes/elder_titan.png',
+    colour: 'green, yellow'
   },
   {
     name: 'Ember Spirit',
-    colour: 'Red, Yellow'
+    icon: '/images/heroes/ember_spirit.png',
+    colour: 'red, yellow'
   },
   {
     name: 'Enchantress',
-    colour: 'Green'
+    icon: '/images/heroes/enchantress.png',
+    colour: 'green'
   },
   {
     name: 'Enigma',
-    colour: 'Purple'
+    icon: '/images/heroes/enigma.png',
+    colour: 'purple'
   },
   {
     name: 'Faceless Void',
-    colour: 'Purple'
+    icon: '/images/heroes/faceless_void.png',
+    colour: 'purple'
   },
   {
     name: 'Gyrocopter',
-    colour: 'Blue, Yellow'
+    icon: '/images/heroes/gyrocopter.png',
+    colour: 'blue, yellow'
   },
   {
     name: 'Huskar',
-    colour: 'Blue, Red'
+    icon: '/images/heroes/huskar.png',
+    colour: 'blue, red'
   },
   {
     name: 'Invoker',
-    colour: 'White'
+    icon: '/images/heroes/invoker.png',
+    colour: 'white'
   },
   {
     name: 'Io',
-    colour: 'White'
+    icon: '/images/heroes/io.png',
+    colour: 'white'
   },
   {
     name: 'Jakiro',
-    colour: 'Blue, Red'
+    icon: '/images/heroes/jakiro.png',
+    colour: 'blue, red'
   },
   {
     name: 'Juggernaut',
-    colour: 'White, Red'
+    icon: '/images/heroes/juggernaut.png',
+    colour: 'white, red'
   },
   {
     name: 'Keeper of the Light',
-    colour: 'White, Yellow'
+    icon: '/images/heroes/keeper_of_the_light.png',
+    colour: 'white, yellow'
   },
-  {,
+  {
     name: 'Kunkka',
-    colour: 'Blue'
+    icon: '/images/heroes/kunkka.png',
+    colour: 'blue'
   },
-  {,
+  {
     name: 'Legion Commander',
-    colour: 'Red, Yellow'
+    icon: '/images/heroes/legion_commander.png',
+    colour: 'red, yellow'
   },
-  {,
+  {
     name: 'Leshrac',
-    colour: 'Blue'
+    icon: '/images/heroes/leshrac.png',
+    colour: 'blue'
   },
   {
     name: 'Lich',
-    colour: 'White'
+    icon: '/images/heroes/lich.png',
+    colour: 'white'
   },
   {
     name: 'Lifestealer',
-    colour: 'Red, Yellow'
+    icon: '/images/heroes/lifestealer.png',
+    colour: 'red, yellow'
   },
   {
     name: 'Lina',
-    colour: 'Red'
+    icon: '/images/heroes/lina.png',
+    colour: 'red'
   },
   {
     name: 'Lion',
-    colour: 'Purple, Red'
+    icon: '/images/heroes/lion.png',
+    colour: 'purple, red'
   },
   {
     name: 'Lone Druid',
-    colour: 'Green, Yellow'
+    icon: '/images/heroes/lone_druid.png',
+    colour: 'green, yellow'
   },
   {
     name: 'Luna',
-    colour: 'Blue'
+    icon: '/images/heroes/luna.png',
+    colour: 'blue'
   },
   {
     name: 'Lycan',
-    colour: 'Red'
+    icon: '/images/heroes/lycan.png',
+    colour: 'red'
   },
   {
     name: 'Magnus',
-    colour: 'Blue'
+    icon: '/images/heroes/magnus.png',
+    colour: 'blue'
   },
   {
     name: 'Medusa',
-    colour: 'Green'
+    icon: '/images/heroes/medusa.png',
+    colour: 'green'
   },
   {
     name: 'Meepo',
-    colour: 'Blue'
+    icon: '/images/heroes/meepo.png',
+    colour: 'blue'
   },
   {
     name: 'Mirana',
-    colour: 'Blue'
+    icon: '/images/heroes/mirana.png',
+    colour: 'blue'
   },
   {
     name: 'Monkey King',
-    colour: 'Red, Yellow'
+    icon: '/images/heroes/monkey_king.png',
+    colour: 'red, yellow'
   },
   {
     name: 'Morphling',
-    colour: 'Blue'
+    icon: '/images/heroes/morphling.png',
+    colour: 'blue'
   },
   {
     name: 'Naga Siren',
-    colour: 'Blue, Yellow'
+    icon: '/images/heroes/naga_siren.png',
+    colour: 'blue, yellow'
   },
   {
     name: 'Nature\'s Prophet',
-    colour: 'Blue, Green'
+    icon: '/images/heroes/natures_prophet.png',
+    colour: 'blue, green'
   },
   {
     name: 'Necrophos',
-    colour: 'Green'
+    icon: '/images/heroes/necrophos.png',
+    colour: 'green'
   },
   {
     name: 'Night Stalker',
-    colour: 'Blue'
+    icon: '/images/heroes/night_stalker.png',
+    colour: 'blue'
   },
   {
     name: 'Nyx Assassin',
-    colour: 'Red, Yellow'
+    icon: '/images/heroes/nyx_assassin.png',
+    colour: 'red, yellow'
   },
   {
     name: 'Ogre Magi',
-    colour: 'Blue, Red'
+    icon: '/images/heroes/ogre_magi.png',
+    colour: 'blue, red'
   },
   {
     name: 'Omniknight',
-    colour: 'Red, White'
+    icon: '/images/heroes/omniknight.png',
+    colour: 'red, white'
   },
   {
     name: 'Oracle',
-    colour: 'Blue, Yellow'
+    icon: '/images/heroes/oracle.png',
+    colour: 'blue, yellow'
   },
   {
     name: 'Outworld Devourer',
-    colour: 'Green'
+    icon: '/images/heroes/outworld_devourer.png',
+    colour: 'green'
   },
   {
     name: 'Pangolier',
-    colour: 'Purple, Yellow'
+    icon: '/images/heroes/pangolier.png',
+    colour: 'purple, yellow'
   },
   {
     name: 'Phantom Assassin',
-    colour: 'Blue'
+    icon: '/images/heroes/phantom_assassin.png',
+    colour: 'blue'
   },
   {
     name: 'Phantom Lancer',
-    colour: 'Blue, Yellow'
+    icon: '/images/heroes/phantom_lancer.png',
+    colour: 'blue, yellow'
   },
   {
     name: 'Phoenix',
-    colour: 'Yellow'
+    icon: '/images/heroes/phoenix.png',
+    colour: 'yellow'
   },
   {
     name: 'Puck',
-    colour: 'Blue, Purple'
+    icon: '/images/heroes/puck.png',
+    colour: 'blue, purple'
   },
   {
     name: 'Pudge',
-    colour: 'Yellow'
+    icon: '/images/heroes/pudge.png',
+    colour: 'yellow'
   },
   {
     name: 'Pugna',
-    colour: 'Green'
+    icon: '/images/heroes/pugna.png',
+    colour: 'green'
   },
   {
     name: 'Queen of Pain',
-    colour: 'Blue, Red'
+    icon: '/images/heroes/queen_of_pain.png',
+    colour: 'blue, red'
   },
   {
     name: 'Razor',
-    colour: 'Blue'
+    icon: '/images/heroes/razor.png',
+    colour: 'blue'
   },
   {
     name: 'Riki',
-    colour: 'Blue, Purple'
+    icon: '/images/heroes/riki.png',
+    colour: 'blue, purple'
   },
   {
     name: 'Rubick',
-    colour: 'Green'
+    icon: '/images/heroes/rubick.png',
+    colour: 'green'
   },
   {
     name: 'Sand King',
-    colour: 'Yellow'
+    icon: '/images/heroes/sand_king.png',
+    colour: 'yellow'
   },
   {
     name: 'Shadow Demon',
-    colour: 'Red, Purple'
+    icon: '/images/heroes/shadow_demon.png',
+    colour: 'red, purple'
   },
   {
     name: 'Shadow Fiend',
-    colour: 'Red'
+    icon: '/images/heroes/shadow_fiend.png',
+    colour: 'red'
   },
   {
     name: 'Shadow Shaman',
-    colour: 'Yellow'
+    icon: '/images/heroes/shadow_shaman.png',
+    colour: 'yellow'
   },
   {
     name: 'Silencer',
-    colour: 'Purple, Yellow'
+    icon: '/images/heroes/silencer.png',
+    colour: 'purple, yellow'
   },
   {
     name: 'Skywrath Mage',
-    colour: 'Yellow, White'
+    icon: '/images/heroes/skywrath_mage.png',
+    colour: 'yellow, white'
   },
   {
     name: 'Slardar',
-    colour: 'Purple'
+    icon: '/images/heroes/slardar.png',
+    colour: 'purple'
   },
   {
     name: 'Slark',
-    colour: 'Blue, Yellow'
+    icon: '/images/heroes/slark.png',
+    colour: 'blue, yellow'
   },
   {
     name: 'Sniper',
-    colour: 'Red, White'
+    icon: '/images/heroes/sniper.png',
+    colour: 'red, white'
   },
   {
     name: 'Spectre',
-    colour: 'Purple'
+    icon: '/images/heroes/spectre.png',
+    colour: 'purple'
   },
   {
     name: 'Spirit Breaker',
-    colour: 'Blue'
+    icon: '/images/heroes/spirit_breaker.png',
+    colour: 'blue'
   },
   {
     name: 'Storm Spirit',
-    colour: 'Blue'
+    icon: '/images/heroes/storm_spirit.png',
+    colour: 'blue'
   },
   {
     name: 'Sven',
-    colour: 'Blue, White'
+    icon: '/images/heroes/sven.png',
+    colour: 'blue, white'
   },
   {
     name: 'Techies',
-    colour: 'Yellow'
+    icon: '/images/heroes/techies.png',
+    colour: 'yellow'
   },
   {
     name: 'Templar Assassin',
-    colour: 'Purple'
+    icon: '/images/heroes/templar_assassin.png',
+    colour: 'purple'
   },
   {
     name: 'Terrorblade',
-    colour: 'Blue, Purple'
+    icon: '/images/heroes/terrorblade.png',
+    colour: 'blue, purple'
   },
   {
     name: 'Tidehunter',
-    colour: 'Green'
+    icon: '/images/heroes/tidehunter.png',
+    colour: 'green'
   },
   {
     name: 'Timbersaw',
-    colour: 'Red, Yellow'
+    icon: '/images/heroes/timbersaw.png',
+    colour: 'red, yellow'
   },
   {
     name: 'Tinker',
-    colour: 'Purple, Yellow'
+    icon: '/images/heroes/tinker.png',
+    colour: 'purple, yellow'
   },
   {
     name: 'Tiny',
-    colour: 'White, Green'
+    icon: '/images/heroes/tiny.png',
+    colour: 'white, green'
   },
   {
     name: 'Treant Protector',
-    colour: 'Green'
+    icon: '/images/heroes/treant_protector.png',
+    colour: 'green'
   },
   {
     name: 'Troll Warlord',
-    colour: 'Red'
+    icon: '/images/heroes/troll_warlord.png',
+    colour: 'red'
   },
   {
     name: 'Tusk',
-    colour: 'Red, White'
+    icon: '/images/heroes/tusk.png',
+    colour: 'red, white'
   },
   {
     name: 'Underlord',
-    colour: 'Green'
+    icon: '/images/heroes/underlord.png',
+    colour: 'green'
   },
   {
     name: 'Undying',
-    colour: 'Green'
+    icon: '/images/heroes/undying.png',
+    colour: 'green'
   },
   {
     name: 'Ursa',
-    colour: 'Blue'
+    icon: '/images/heroes/ursa.png',
+    colour: 'blue'
   },
   {
     name: 'Vengeful Spirit',
-    colour: 'Blue'
+    icon: '/images/heroes/vengeful_spirit.png',
+    colour: 'blue'
   },
   {
     name: 'Venomancer',
-    colour: 'Green'
+    icon: '/images/heroes/venomancer.png',
+    colour: 'green'
   },
   {
     name: 'Viper',
-    colour: 'Green'
+    icon: '/images/heroes/viper.png',
+    colour: 'green'
   },
   {
     name: 'Visage',
-    colour: 'Blue'
+    icon: '/images/heroes/visage.png',
+    colour: 'blue'
   },
   {
     name: 'Warlock',
-    colour: 'Red'
+    icon: '/images/heroes/warlock.png',
+    colour: 'red'
   },
   {
     name: 'Weaver',
-    colour: 'Blue'
+    icon: '/images/heroes/weaver.png',
+    colour: 'blue'
   },
   {
     name: 'Windranger',
-    colour: 'Green'
+    icon: '/images/heroes/windranger.png',
+    colour: 'green'
   },
   {
     name: 'Winter Wyvern',
-    colour: 'Blue, White'
+    icon: '/images/heroes/winter_wyvern.png',
+    colour: 'blue, white'
   },
   {
     name: 'Witch Doctor',
-    colour: 'Purple'
+    icon: '/images/heroes/witch_doctor.png',
+    colour: 'purple'
   },
   {
     name: 'Wraith King',
-    colour: 'Green'
+    icon: '/images/heroes/wraith_king.png',
+    colour: 'green'
   },
   {
     name: 'Zeus',
-    colour: 'Blue, White'
+    icon: '/images/heroes/zeus.png',
+    colour: 'blue, white'
   }
 
 
 );
-console.log(heroes);
+var bhtml = "";
+var ghtml = "";
+var phtml = "";
+var rhtml = "";
+var whtml = "";
+var yhtml = "";
+
+function addColourHero(html, hero){
+    return html + '<div class="row "><div class="col-md-4"><img width="64" height="36" src="' + hero.icon + '"></div><div class="col-md-8">' + hero.name + '</div></div>'
+}
+
+heroes.forEach(function(hero){
+  if(hero.colour.includes('blue')){
+    bhtml = addColourHero(bhtml, hero);
+  }
+  if(hero.colour.includes('green')){
+    ghtml = addColourHero(ghtml, hero);
+  }
+  if(hero.colour.includes('purple')){
+    phtml = addColourHero(phtml, hero);
+  }
+  if(hero.colour.includes('red')){
+    rhtml = addColourHero(rhtml, hero);
+  }
+  if(hero.colour.includes('white')){
+    whtml = addColourHero(whtml, hero);
+  }
+  if(hero.colour.includes('yellow')){
+    yhtml = addColourHero(yhtml, hero);
+  }
+});
+
+$('#blue').html(bhtml);
+$('#green').html(ghtml);
+$('#purple').html(phtml);
+$('#red').html(rhtml);
+$('#white').html(whtml);
+$('#yellow').html(yhtml);
